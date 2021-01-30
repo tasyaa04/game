fe8f335a4976953ab484c403783df6c5a70f1797
R1fle <boshok@rambler.ru>
2021-01-30 15:08:47 +0300

    Platform interacting updated. Added the possibility of motion.

--- player.py
+++ player.py
@@ -39,18 +39,24 @@ class Player(pygame.sprite.Sprite):
         self.level = None
         self.lives = 3
 
+        # used to manage interacting with the platforms
+        self.in_air = False
+        self.in_fall = True
+        self.in_air_secs = 0
+
     def update(self):
         """Move the player"""
-
-        self.rect.x += self.speed_x
-        self.rect.y += self.speed_y
+        keys = pygame.key.get_pressed()
 
         sprites = pygame.sprite.spritecollide(self, self.level.platforms, False)
         if not sprites:
-            self.speed_y -= 0
+            self.speed_y += 20
         else:
-            # self.rect.bottom = sprites[0].rect.top
-            pass
+            self.in_fall = False
+            if not self.in_fall and not self.in_air and self.rect.bottom >= sprites[0].rect.top:
+                self.rect.bottom = sprites[0].rect.top + 20
+            else:
+                self.speed_y += 20
 
         # if the player hits the enemies he lose one live
         if pygame.sprite.spritecollide(self, self.level.enemies, False):
@@ -59,12 +65,31 @@ class Player(pygame.sprite.Sprite):
             if self.lives < 1:
                 running = False
 
+        if keys[pygame.K_LEFT]:
+            self.left_move()
+        if keys[pygame.K_RIGHT]:
+            self.right_move()
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+        if keys[pygame.K_UP] and self.current_animation not in (self.jump_r, self.jump_l):
+            self.jump()
+
+        if self.in_air and self.in_air_secs > 0:
+            self.rect.y -= 30
+            self.in_air_secs -= 1
+            self.in_air = False if self.in_air_secs == 0 else self.in_air
+
+        self.update_animation(keys)
+
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+
+        self.in_fall = True if self.speed_y > 0 else False
+
+        self.speed_x = 0
         self.speed_y = 0
 
-        self.update_animation()
-
-    def update_animation(self):
-        keys = pygame.key.get_pressed()
+    def update_animation(self, keys):
 
         # changing direction of player
         if keys[pygame.K_LEFT]:
@@ -73,7 +98,7 @@ class Player(pygame.sprite.Sprite):
             self.player_facing = 'r'
 
         # changing animation depending on pressed keys
-        if keys[pygame.K_UP] or not pygame.sprite.spritecollide(self, self.level.platforms, False):
+        if self.in_fall or self.in_air:
             self.current_animation = self.jump_r if self.player_facing == 'r' else self.jump_l
         elif keys[pygame.K_SPACE] and (keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]):
             self.current_animation = self.run_shoot_r \
@@ -92,3 +117,16 @@ class Player(pygame.sprite.Sprite):
                                                 len(self.current_animation)]
         except ValueError:
             self.image = self.current_animation[0]
+
+    def left_move(self):
+        self.speed_x -= 20
+
+    def right_move(self):
+        self.speed_x += 20
+
+    def jump(self):
+        self.in_air = True
+        self.in_air_secs = 25
+
+    def shoot(self):
+        pass
